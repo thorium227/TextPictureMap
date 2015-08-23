3e5125b185c3f5a2d26ba325c0a392406beae901
Throrium <thorium232.10@gmail.com>
2015-08-23 14:37:47 +0300

    initial commit

---
+++ TextPictureMap/BitmapTextConverter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextPictureMap
+{
+    public class BitmapTextConverter
+    {
+        public const int BITMAP_DATA_OFFSET = 54;
+        private const int BMP_WIDTH = 350;
+        private const int BMP_HEIGHT = 350;
+
+        private static byte[] GetNewBitmapBytes()
+        {
+            Bitmap pic = new Bitmap(BMP_WIDTH, BMP_HEIGHT);
+            using(Graphics gr = Graphics.FromImage(pic))
+            {
+                Rectangle imgBgr = new Rectangle(0, 0, BMP_WIDTH, BMP_HEIGHT);
+                gr.FillRectangle(Brushes.White, imgBgr);
+            }
+
+            var picBytes = pic.ToByteArray(ImageFormat.Bmp);
+
+            return picBytes;
+        }
+
+        public static void GenerateTextBitmap(string text, Encoding encoding, string outputPath)
+        {
+            byte[] bytesToReplace = encoding.GetBytes(text);
+
+            byte[] bitmapBytes = GetNewBitmapBytes();
+            if (bytesToReplace.LongLength > (bitmapBytes.LongLength - BITMAP_DATA_OFFSET - 1))
+                throw new ArgumentException("Text too big");            
+
+            for (long i = 0; i < bytesToReplace.LongLength; i++)
+            {
+                bitmapBytes[i + BITMAP_DATA_OFFSET] = bytesToReplace[i];
+            }
+
+            File.WriteAllBytes(outputPath, bitmapBytes);
+        }
+
+        public static void GenerateTextBitmapFromFile(string path, Encoding encoding, string outputPath)
+        {
+            string text = File.ReadAllText(path);
+
+            GenerateTextBitmap(text, encoding, outputPath);
+        }
+    }
+}
---
+++ TextPictureMap/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextPictureMap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 4)
+            {
+                PrintHelp();
+                return;
+            }
+
+            string mode = args[0];
+
+            if(mode == "img")
+            {
+                string sourcePathOrString = args[1];
+                string outputPath = args[4];
+
+                Encoding textEncoding = Encoding.GetEncoding(args[2]);
+
+                BitmapTextConverter.GenerateTextBitmapFromFile(sourcePathOrString, textEncoding, outputPath);
+            }
+            else if(mode == "text")
+            {
+                string sourcePathOrString = args[1];
+                string outputPath = args[3];
+
+                throw new NotImplementedException("Functionality not available yet");
+            }
+            else
+            {
+                Console.WriteLine("Unknown mode");
+                return;
+            }
+
+            //Bitmap pic = new Bitmap(350, 350);
+
+            //var picBytes = pic.ToByteArray(ImageFormat.Bmp);
+
+            //Console.WriteLine(picBytes.Length);
+
+            Console.WriteLine("Image generation finished");
+
+            Console.Read();
+        }
+
+        static void PrintHelp()
+        {
+            Console.WriteLine(AppDomain.CurrentDomain.FriendlyName + " [mode|source path (encoding)|-o|ouput path]");
+            Console.WriteLine("e.g.");
+            Console.WriteLine(AppDomain.CurrentDomain.FriendlyName + @" img C:\tmp\hamlet.txt ascii -o C:\tmp\example.bmp");
+            Console.WriteLine(AppDomain.CurrentDomain.FriendlyName + @" text C:\tmp\example.bmp -o C:\tmp\hamlet.txt");
+        }
+    }
+}
